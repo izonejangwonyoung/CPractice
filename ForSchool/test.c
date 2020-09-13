bb3ae3112af84fe2a15f3ca982ace26d89fb3b99
izonejangwonyoung <2000shim@naver.com>
2020-09-13 15:25:41 +0900

    during test...

--- ForSchool/test.c
+++ ForSchool/test.c
@@ -7,6 +7,6 @@ int main(void)
 
 
     printf("hello world");
-
+    printf("hello world");
     return 0;
 }
---
+++ OnlyMe/ff.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+
+
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+    printf("helloss world");
+
+    return 0;
+}
